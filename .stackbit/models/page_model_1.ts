5b02dccdd590960a4562780294dbf772f4a32e07
rodik-dev <42075304+rodik-dev@users.noreply.github.com>
2024-03-21 20:45:16 +0200

    initial permissions

--- .stackbit/models/page_model_1.ts
+++ .stackbit/models/page_model_1.ts
@@ -7,7 +7,21 @@ export const page_model_1: Model = {
         return `content/pages/${data.slug.replace(/\W+/g, '-')}.md`;
     },
     hideContent: false,
-    fields: [
+    permissions: (options) => {
+        // allow a specific role to edit but not publish
+        if (options.userContext.sso?.attributes?.roles.includes('data_model_1_editor_role')) {
+            return { canView: true, canEdit: true, canPublish: false };
+        }
+
+        // allow a specific role to edit and publish
+        if (options.userContext.sso?.attributes?.roles.includes('data_model_1_publisher_role')) {
+            return { canView: true, canEdit: true, canPublish: true };
+        }
+
+        // by default, no one else can edit or publish
+        return { canView: true, canEdit: false, canPublish: false };
+    },
+   fields: [
         {
             type: 'string',
             name: 'title'
--- .stackbit/models/page_model_2.ts
+++ .stackbit/models/page_model_2.ts
@@ -5,7 +5,21 @@ export const page_model_2: Model = {
     type: 'page',
     filePath: 'content/pages/{slug}.md',
     hideContent: true,
-    fields: [
+    permissions: (options) => {
+        // allow a specific role to edit but not publish
+        if (options.userContext.sso?.attributes?.roles.includes('data_model_2_editor_role')) {
+            return { canView: true, canEdit: true, canPublish: false };
+        }
+
+        // allow a specific role to edit and publish
+        if (options.userContext.sso?.attributes?.roles.includes('data_model_2_publisher_role')) {
+            return { canView: true, canEdit: true, canPublish: true };
+        }
+
+        // by default, no one else can view the model or it's documents
+        return { canView: false, canEdit: false, canPublish: false };
+    },
+  fields: [
         {
             type: 'string',
             name: 'string_field'
